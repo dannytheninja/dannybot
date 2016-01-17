131622f6adc4565bd67db5fda2644948c89f5958
DannyTheNinja <danny@paddedninja.net>
2016-01-16 22:59:17 -0500

    IRC\Client: code style cleanup

--- src/IRC/Client.php
+++ src/IRC/Client.php
@@ -28,14 +28,14 @@ namespace DannyTheNinja\IRC;
 class Client
 {
 	private $socket;
-	private $hooks = array();
+	private $hooks = [];
 	private $identity;
 	
 	// List of channels we're joined to, and information about them
-	protected $joined_channels = array();
+	protected $joined_channels = [];
 	
 	// whois data cache
-	protected $whois_cache = array();
+	protected $whois_cache = [];
 	
 	// Regular expression fragments, used to ease readability of message parser code
 	const RE_FRAG_HOSTNAME = '(?:(?:[a-z0-9-]+\.)*(?:[a-z0-9-]+)|(?:[0-9a-z:]+))';
@@ -48,7 +48,7 @@ class Client
 	
 	public function __get($name)
 	{
-		if ( isset($this->$name) && !in_array($name, array('socket', 'hooks')) )
+		if ( isset($this->$name) && !in_array($name, ['socket', 'hooks']) )
 			return $this->$name;
 	}
 	
@@ -91,12 +91,12 @@ class Client
 	 * @param array SSL context options
 	 */
 	
-	public function connect($host, $port, $identity, $ssl = false, $ssl_options = array())
+	public function connect($host, $port, $identity, $ssl = false, $ssl_options = [])
 	{
 		if ( $ssl ) {
-			$sslctx = stream_context_create(array(
+			$sslctx = stream_context_create([
 					'ssl' => $ssl_options
-				));
+				]);
 			$host = "tls://$host";
 			$this->socket = stream_socket_client("$host:$port", $errno, $errstr, ini_get('default_socket_timeout'), STREAM_CLIENT_CONNECT, $sslctx);
 		}
@@ -129,79 +129,76 @@ class Client
 			});
 		
 		// channel join handler
-		$this->bind('JOIN', function($irc, $msg)
-			{
+		$this->bind(
+			'JOIN',
+			function($irc, $msg) {
 				$channel = $msg['body'];
 				
-				if ( $msg['identity']['nick'] === $this->identity['nick'] )
-				{
+				if ( $msg['identity']['nick'] === $this->identity['nick'] ) {
 					// log that the channel was joined
 					$irc->info("Channel $channel was joined successfully.");
-					$irc->joined_channels[$channel] = array('names' => array());
+					$irc->joined_channels[$channel] = [ 'names' => [] ];
 					
 					// set a composite hook for the NAMES list
-					$irc->bind(array(Opcode::OP_NAMES_LIST, Opcode::OP_NAMES_END), function($irc, $msg) use ($channel)
-						{
+					$irc->bind(
+						[ Opcode::OP_NAMES_LIST, Opcode::OP_NAMES_END ],
+						function($irc, $msg) use ($channel) {
 							// verify that this line pertains to this channel
 							$mchannel = preg_replace("/^{$this->identity['nick']} (= )?/", '', $msg['extra']);
-							if ( $mchannel !== $channel )
+							if ( $mchannel !== $channel ) {
 								return;
+							}
 							
-							if ( $msg['opcode'] === Opcode::OP_NAMES_END )
-							{
+							if ( $msg['opcode'] === Opcode::OP_NAMES_END ) {
 								throw new Signal\Unhook();
 							}
 							
-							foreach ( preg_split('/\s+/', $msg['body']) as $nick )
-							{
+							foreach ( preg_split('/\s+/', $msg['body']) as $nick ) {
 								$irc->joined_channels[$channel]['names'][] = preg_replace('/^[@\+&~%]/', '', $nick);
 							}
 						});
 				}
-				else
-				{
+				else {
 					$this->joined_channels[$channel]['names'][] = $msg['identity']['nick'];
 				}
 			});
 		
 		// channel part/kick handlers
-		$this->bind('PART', function($irc, $msg)
-			{
+		$this->bind(
+			'PART',
+			function($irc, $msg) {
 				$channel =& $msg['extra'];
-				if ( $msg['identity']['nick'] === $irc->identity['nick'] )
-				{
+				if ( $msg['identity']['nick'] === $irc->identity['nick'] ) {
 					// we left
 					unset($irc->joined_channels[$channel]);
 				}
-				else
-				{
+				else {
 					// someone else left
-					foreach ( $irc->joined_channels[$channel]['names'] as $i => $name )
-					{
-						if ( $name === $msg['identity']['nick'] )
+					foreach ( $irc->joined_channels[$channel]['names'] as $i => $name ) {
+						if ( $name === $msg['identity']['nick'] ) {
 							unset($irc->joined_channels[$channel]['names'][$i]);
+						}
 					}
 					
 					$irc->joined_channels[$channel]['names'] = array_values($irc->joined_channels[$channel]['names']);
 				}
 			});
 		
-		$this->bind('KICK', function($irc, $msg)
-			{
+		$this->bind(
+			'KICK',
+			function($irc, $msg) {
 				list($channel, $who) = preg_split('/\s+/', $msg['extra']);
 				
-				if ( $who === $irc->identity['nick'] )
-				{
+				if ( $who === $irc->identity['nick'] ) {
 					// we were kicked
 					unset($irc->joined_channels[$channel]);
 				}
-				else
-				{
+				else {
 					// someone else left
-					foreach ( $irc->joined_channels[$channel]['names'] as $i => $name )
-					{
-						if ( $name === $who )
+					foreach ( $irc->joined_channels[$channel]['names'] as $i => $name ) {
+						if ( $name === $who ) {
 							unset($irc->joined_channels[$channel]['names'][$i]);
+						}
 					}
 					
 					$irc->joined_channels[$channel]['names'] = array_values($irc->joined_channels[$channel]['names']);
@@ -209,16 +206,13 @@ class Client
 			});
 		
 		// quit handler - for when a user quits IRC
-		$this->bind('QUIT', function($irc, $msg)
-			{
-				foreach ( $irc->joined_channels as $channame => $channel )
-				{
-					if ( in_array($msg['identity']['nick'], $channel['names']) )
-					{
-						foreach ( $channel['names'] as $i => $name )
-						{
-							if ( $msg['identity']['nick'] === $name )
-							{
+		$this->bind(
+			'QUIT',
+			function($irc, $msg) {
+				foreach ( $irc->joined_channels as $channame => $channel ) {
+					if ( in_array($msg['identity']['nick'], $channel['names']) ) {
+						foreach ( $channel['names'] as $i => $name ) {
+							if ( $msg['identity']['nick'] === $name ) {
 								$irc->info("$name has quit IRC, removing them from $channame");
 								unset($channel['names'][$i]);
 							}
@@ -228,10 +222,10 @@ class Client
 			});
 		
 		// nick changes
-		$this->bind('NICK', function($irc, $msg)
-			{
-				if ( $msg['identity']['nick'] === $irc->identity['nick'] )
-				{
+		$this->bind(
+			'NICK',
+			function($irc, $msg) {
+				if ( $msg['identity']['nick'] === $irc->identity['nick'] ) {
 					// our own nick changed
 					$irc->identity['nick'] = $msg['body'];
 				}
@@ -239,12 +233,11 @@ class Client
 				$oldnick =& $msg['identity']['nick'];
 				$newnick =& $msg['body'];
 				
-				foreach ( $this->joined_channels as &$chan )
-				{
-					foreach ( $chan['names'] as &$name )
-					{
-						if ( $name === $oldnick )
+				foreach ( $this->joined_channels as &$chan ) {
+					foreach ( $chan['names'] as &$name ) {
+						if ( $name === $oldnick ) {
 							$name = $newnick;
+						}
 					}
 					unset($name);
 				}
@@ -270,8 +263,9 @@ class Client
 	
 	public function join($channel)
 	{
-		if ( isset($this->joined_channels[$channel]) )
+		if ( isset($this->joined_channels[$channel]) ) {
 			return;
+		}
 		
 		$this->write("JOIN $channel");
 	}
@@ -283,8 +277,9 @@ class Client
 	
 	public function part($channel, $reason = '')
 	{
-		if ( !isset($this->joined_channels[$channel]) )
+		if ( !isset($this->joined_channels[$channel]) ) {
 			return;
+		}
 		
 		$this->write("PART $channel :$reason");
 	}
@@ -297,8 +292,9 @@ class Client
 	
 	public function privmsg($target, $msg)
 	{
-		if ( strstr($msg, "\r") )
+		if ( strstr($msg, "\r") ) {
 			throw new ClientException("Carriage returns are not permitted in private messages");
+		}
 		
 		foreach ( explode("\n", trim($msg)) as $line )
 		{
@@ -314,36 +310,30 @@ class Client
 	
 	public function event_loop()
 	{
-		while ( !feof($this->socket) )
-		{
-			try
-			{
+		while ( !feof($this->socket) ) {
+			try {
 				$line = fgets($this->socket, 1024);
-				if ( substr($line, -2) === "\r\n" )
-				{
+				if ( substr($line, -2) === "\r\n" ) {
 					// it's a message!
 					$line = trim($line);
-					if ( $msg = $this->parse_message($line) )
-					{
+					if ( $msg = $this->parse_message($line) ) {
 						echo "\x1B[0;32;1m<-\x1B[0m $line\n";
 						$this->run_hooks($msg['type'], $msg['opcode'], $msg);
 					}
-					else
-					{
+					else {
 						echo "\x1B[0;31;1m<-\x1B[0m $line\n";
 					}
 				}
 			}
-			catch ( Signal\Quit $iqs )
-			{
+			catch ( Signal\Quit $iqs ) {
 				$this->write("QUIT :{$iqs->getQuitMessage()}");
 				break;
 			}
-			catch ( Signal\BreakLoop $ibls )
-			{
+			catch ( Signal\BreakLoop $ibls ) {
 				return;
 			}
 		}
+		
 		fclose($this->socket);
 		$this->socket = false;
 	}
@@ -356,26 +346,22 @@ class Client
 	private function run_hooks($type, $opcode, $msg)
 	{
 		// Run all simple hooks.
-		if ( isset($this->hooks[$opcode]) )
-		{
-			foreach ( $this->hooks[$opcode] as $i => $func )
-			{
-				if ( !is_callable($func) )
+		if ( isset($this->hooks[$opcode]) ) {
+			foreach ( $this->hooks[$opcode] as $i => $func ) {
+				if ( !is_callable($func) ) {
 					continue;
+				}
 				
-				try
-				{
+				try {
 					call_user_func($func, $this, $msg);
 				}
-				catch ( Signal\Unhook $ius )
-				{
+				catch ( Signal\Unhook $ius ) {
 					// "IRCUnhookSignal" will cause the hook to be removed. The concept of temporary hooks
 					// allows the introduction of polling loops that grab a finite amount of information, then
 					// are finished.
 					unset($this->hooks[$opcode][$i]);
 				}
-				catch ( Signal\BreakHooks $ibh )
-				{
+				catch ( Signal\BreakHooks $ibh ) {
 					// "IRCBreakHooks" will cause all further hooks for this event to stop being processed. This
 					// should only be used in cases where you are, for example, handling user input and responding
 					// to a command.
@@ -386,26 +372,21 @@ class Client
 		}
 		
 		// Run all composite hooks (hooks bound to more than one opcode)
-		if ( isset($this->hooks['composite']) )
-		{
-			foreach ( $this->hooks['composite'] as $i => $hook )
-			{
-				if ( in_array($opcode, $hook['opcodes']) )
-				{
-					if ( !is_callable($hook['function']) )
+		if ( isset($this->hooks['composite']) ) {
+			foreach ( $this->hooks['composite'] as $i => $hook ) {
+				if ( in_array($opcode, $hook['opcodes']) ) {
+					if ( !is_callable($hook['function']) ) {
 						continue;
+					}
 					
 					// Same deal as before
-					try
-					{
+					try {
 						call_user_func($hook['function'], $this, $msg);
 					}
-					catch ( Signal\Unhook $ius )
-					{
+					catch ( Signal\Unhook $ius ) {
 						unset($this->hooks['composite'][$i]);
 					}
-					catch ( Signal\BreakHooks $ibh )
-					{
+					catch ( Signal\BreakHooks $ibh ) {
 						break;
 					}
 				}
@@ -423,80 +404,74 @@ class Client
 	
 	private function parse_message($line)
 	{
-		if ( preg_match('/^:(' . self::RE_FRAG_HOSTNAME . ') (\d+|[A-Z]+) (.*?) :(.*)$/', $line, $match) )
-		{
-			return array(
-					'type' => 'server'
-					, 'line' => $match[0]
-					, 'server' => $match[1]
-					, 'opcode' => $match[2]
-					, 'extra' => $match[3]
-					, 'body' => $match[4]
-				);
+		if ( preg_match('/^:(' . self::RE_FRAG_HOSTNAME . ') (\d+|[A-Z]+) (.*?) :(.*)$/', $line, $match) ) {
+			return [
+				'type' => 'server',
+				'line' => $match[0],
+				'server' => $match[1],
+				'opcode' => $match[2],
+				'extra' => $match[3],
+				'body' => $match[4]
+			];
 		}
-		else if ( preg_match('/^:(' . self::RE_FRAG_HOSTNAME . ') (\d+|[A-Z]+) (.*?)$/', $line, $match) )
-		{
-			return array(
-					'type' => 'server'
-					, 'line' => $match[0]
-					, 'server' => $match[1]
-					, 'opcode' => $match[2]
-					, 'extra' => $match[3]
-					, 'body' => false
-				);
+		else if ( preg_match('/^:(' . self::RE_FRAG_HOSTNAME . ') (\d+|[A-Z]+) (.*?)$/', $line, $match) ) {
+			return [
+				'type' => 'server',
+				'line' => $match[0],
+				'server' => $match[1],
+				'opcode' => $match[2],
+				'extra' => $match[3],
+				'body' => false
+			];
 		}
-		else if ( preg_match('/^:(' . self::RE_FRAG_NICK . ')!(' . self::RE_FRAG_USERNAME . ')@(' . self::RE_FRAG_HOSTNAME . ') ([A-Z]+) (.*?) :(.*)$/', $line, $match) )
-		{
-			return array(
-					'type' => 'user'
-					, 'line' => $match[0]
-					, 'identity' => array(
-						'nick' => $match[1]
-						, 'user' => $match[2]
-						, 'hostname' => $match[3]
-						)
-					, 'opcode' => $match[4]
-					, 'extra' => $match[5]
-					, 'body' => $match[6]
-				);
+		else if ( preg_match('/^:(' . self::RE_FRAG_NICK . ')!(' . self::RE_FRAG_USERNAME . ')@(' . self::RE_FRAG_HOSTNAME . ') ([A-Z]+) (.*?) :(.*)$/', $line, $match) ) {
+			return [
+				'type' => 'user',
+				'line' => $match[0],
+				'identity' => [
+					'nick' => $match[1],
+					'user' => $match[2],
+					'hostname' => $match[3]
+				],
+				'opcode' => $match[4],
+				'extra' => $match[5],
+				'body' => $match[6]
+			];
 		}
-		else if ( preg_match('/^:(' . self::RE_FRAG_NICK . ')!(' . self::RE_FRAG_USERNAME . ')@(' . self::RE_FRAG_HOSTNAME . ') ([A-Z]+) :(.*)$/', $line, $match) )
-		{
-			return array(
-					'type' => 'user'
-					, 'line' => $match[0]
-					, 'identity' => array(
-						'nick' => $match[1]
-						, 'user' => $match[2]
-						, 'hostname' => $match[3]
-						)
-					, 'opcode' => $match[4]
-					, 'extra' => false
-					, 'body' => $match[5]
-				);
+		else if ( preg_match('/^:(' . self::RE_FRAG_NICK . ')!(' . self::RE_FRAG_USERNAME . ')@(' . self::RE_FRAG_HOSTNAME . ') ([A-Z]+) :(.*)$/', $line, $match) ) {
+			return [
+				'type' => 'user',
+				'line' => $match[0],
+				'identity' => [
+					'nick' => $match[1],
+					'user' => $match[2],
+					'hostname' => $match[3]
+				],
+				'opcode' => $match[4],
+				'extra' => false,
+				'body' => $match[5]
+			];
 		}
-		else if ( preg_match('/^:(' . self::RE_FRAG_NICK . ')!(' . self::RE_FRAG_USERNAME . ')@(' . self::RE_FRAG_HOSTNAME . ') ([A-Z]+) (.*?)$/', $line, $match) )
-		{
-			return array(
-					'type' => 'user'
-					, 'line' => $match[0]
-					, 'identity' => array(
-						'nick' => $match[1]
-						, 'user' => $match[2]
-						, 'hostname' => $match[3]
-						)
-					, 'opcode' => $match[4]
-					, 'extra' => $match[5]
-				);
+		else if ( preg_match('/^:(' . self::RE_FRAG_NICK . ')!(' . self::RE_FRAG_USERNAME . ')@(' . self::RE_FRAG_HOSTNAME . ') ([A-Z]+) (.*?)$/', $line, $match) ) {
+			return [
+				'type' => 'user',
+				'line' => $match[0],
+				'identity' => [
+					'nick' => $match[1],
+					'user' => $match[2],
+					'hostname' => $match[3]
+				],
+				'opcode' => $match[4],
+				'extra' => $match[5]
+			];
 		}
-		else if ( preg_match('/^([A-Z]+) :(.*)$/', $line, $match) )
-		{
-			return array(
-					'type' => 'primitive'
-					, 'line' => $match[0]
-					, 'opcode' => $match[1]
-					, 'body' => $match[2]
-				);
+		else if ( preg_match('/^([A-Z]+) :(.*)$/', $line, $match) ) {
+			return [
+				'type' => 'primitive',
+				'line' => $match[0],
+				'opcode' => $match[1],
+				'body' => $match[2]
+			];
 		}
 		
 		return false;
@@ -513,22 +488,22 @@ class Client
 	
 	public function bind($opcode, $function)
 	{
-		if ( is_array($opcode) )
-		{
+		if ( is_array($opcode) ) {
 			// composite hook - bound to multiple opcodes
-			if ( !isset($this->hooks['composite']) )
+			if ( !isset($this->hooks['composite']) ) {
 				$this->hooks['composite'] = array();
+			}
 			
-			$this->hooks['composite'][] = array(
-					'opcodes' => $opcode
-					, 'function' => $function
-				);
+			$this->hooks['composite'][] = [
+				'opcodes' => $opcode,
+				'function' => $function
+			];
 		}
-		else
-		{
+		else {
 			// simple hook - bound to one opcode
-			if ( !isset($this->hooks[$opcode]) )
-				$this->hooks[$opcode] = array();
+			if ( !isset($this->hooks[$opcode]) ) {
+				$this->hooks[$opcode] = [];
+			}
 			
 			$this->hooks[$opcode][] = $function;
 		}
@@ -542,8 +517,7 @@ class Client
 	
 	public function whois($nick, $callback)
 	{
-		if ( isset($this->whois_cache[$nick]) && $this->whois_cache[$nick]['timestamp'] + 300 > time() )
-		{
+		if ( isset($this->whois_cache[$nick]) && $this->whois_cache[$nick]['timestamp'] + 300 > time() ) {
 			call_user_func($callback, $this->whois_cache[$nick]);
 			return;
 		}
@@ -554,46 +528,43 @@ class Client
 		
 		$this->whois_cache[$nick] = array();
 		
-		$this->bind(array(
-				Opcode::OP_WHOIS_IDENTITY
-				, Opcode::OP_WHOIS_CHANNELS
-				, Opcode::OP_WHOIS_SERVER
-				, Opcode::OP_WHOIS_SERVICES_IDENTITY
-				, Opcode::OP_WHOIS_END
-			),
-			function($irc, $msg) use ($callback, $nick)
-			{
-				switch($msg['opcode'])
-				{
+		$this->bind([
+				Opcode::OP_WHOIS_IDENTITY,
+				Opcode::OP_WHOIS_CHANNELS,
+				Opcode::OP_WHOIS_SERVER,
+				Opcode::OP_WHOIS_SERVICES_IDENTITY,
+				Opcode::OP_WHOIS_END
+			],
+			function($irc, $msg) use ($callback, $nick) {
+				switch($msg['opcode']) {
 					case Opcode::OP_WHOIS_IDENTITY:
 						list(,$snick,$user,$hostname) = preg_split('/\s+/', $msg['extra']);
 						$gecos = $msg['body'];
-						$irc->whois_cache[$nick]['identity'] = array(
-								'nick' => $snick
-								, 'user' => $user
-								, 'hostname' => $hostname
-								, 'gecos' => $gecos
-							);
+						$irc->whois_cache[$nick]['identity'] = [
+							'nick' => $snick,
+							'user' => $user,
+							'hostname' => $hostname,
+							'gecos' => $gecos
+						];
 						
 						break;
 					case Opcode::OP_WHOIS_CHANNELS:
-						foreach ( preg_split('/\s+/', $msg['body']) as $channel )
-						{
+						foreach ( preg_split('/\s+/', $msg['body']) as $channel ) {
 							$irc->whois_cache[$nick]['channels'][] = $channel;
 						}
 						break;
 					case Opcode::OP_WHOIS_SERVER:
 						list(,,$server) = preg_split('/\s+/', $msg['extra']);
-						$irc->whois_cache[$nick]['server'] = array(
-								'hostname' => $server
-								, 'location' => $msg['body']
-							);
+						$irc->whois_cache[$nick]['server'] = [
+							'hostname' => $server,
+							'location' => $msg['body']
+						];
 						break;
 					case Opcode::OP_WHOIS_SERVICES_IDENTITY:
 						list(,,$idnick) = preg_split('/\s+/', $msg['extra']);
-						$irc->whois_cache[$nick]['services_identity'] = array(
-								'idnick' => $idnick
-							);
+						$irc->whois_cache[$nick]['services_identity'] = [
+							'idnick' => $idnick
+						];
 						break;
 					case Opcode::OP_WHOIS_END:
 						$irc->whois_cache[$nick]['timestamp'] = time();
@@ -619,20 +590,19 @@ class Client
 		$text = preg_replace('#<b>(.*?)</b>#is', "\x02$1\x02", $text);
 		$text = preg_replace('#<u>(.*?)</u>#is', "\x1f$1\x1f", $text);
 		
-		if ( preg_match('#^/me #i', $text) )
-		{
+		if ( preg_match('#^/me #i', $text) ) {
 			$text = "\x01ACTION " . preg_replace('#^/me #i', '', $text) . "\x01";
 		}
 		
-		$supportedcolors = array('white', 'black', 'navy', 'green', 'red', 'maroon', 'purple', 'orange', 'yellow', 'lime', 'teal',
-			'cyan', 'blue', 'pink', 'grey', 'silver');
+		$supportedcolors = ['white', 'black', 'navy', 'green', 'red', 'maroon',
+			'purple', 'orange', 'yellow', 'lime', 'teal', 'cyan', 'blue',
+			'pink', 'grey', 'silver'];
 		
 		$colors = implode('|', $supportedcolors);
 		$supportedcolors = array_flip($supportedcolors);
 		preg_match_all("#<((?:$colors)(?::(?:$colors))?)>(.*?)</\\1>#is", $text, $matches);
 		
-		foreach ( $matches[0] as $i => $match )
-		{
+		foreach ( $matches[0] as $i => $match ) {
 			preg_match("#<(?:($colors)(?::($colors))?)>#i", $match, $colordata);
 			$fgcolor = $supportedcolors[$colordata[1]];
 			$bgcolor = $colordata[1] ? $supportedcolors[$colordata[2]] : '';
@@ -646,8 +616,9 @@ class Client
 	}
 	
 	/**
-	 * "QUOTE" - frontend for write(). Throws IRCIKnowThisIsDangerousException unconditionally.
-	 * @param string Line to write
+	 * "QUOTE" - frontend for write().
+	 * @param string
+	 *   Line to write
 	 */
 	
 	public function quote($line)
@@ -683,11 +654,9 @@ class Client
 	
 	public function __destruct()
 	{
-		if ( $this->socket )
-		{
-			$this->write("QUIT :IRCClient instance is being destroyed");
-			while ( !feof($this->socket) )
-			{
+		if ( $this->socket ) {
+			$this->write("QUIT :IRC\\Client instance is being destroyed");
+			while ( !feof($this->socket) ) {
 				fgets($this->socket, 1024);
 			}
 			
